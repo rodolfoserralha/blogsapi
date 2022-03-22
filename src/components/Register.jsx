fe22f58620e2eba44586ecc70a7ecad3fa7bfd91
Rodolfo <rodolfoserralha@hotmail.com>
2022-03-22 18:56:51 -0300

    Feat: add fetch to login and authentication

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,25 +1,47 @@
 import React, { useState } from 'react';
 
-export default function Register() {
+function RightLogin() {
   const [register, setRegister] = useState(false);
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleOnClick() {
+    setRegister(!register)
+
+    if (register === true) {
+      alert('Usuário cadastrado com sucesso')
+    }
+  }
 
   return (
     <div className="right-side-login">
-      <h2 className="welcome">Digite os dados</h2>
+      <h2 className="welcome">{ register === false ? 'Seja bem-vindo' : 'Digite os dados' }</h2>
       <div className="inputs">
         <label className="label-input" htmlFor="user">Usuário</label>
-        <input className="input-login" id="user" type="text" />
+        <input
+          className="input-login" 
+          name="user" 
+          id="user" 
+          type="text" 
+          onChange={ (e) => setUser(e.target.value) }
+        />
       </div>
       <div className="inputs">
         <label className="label-input" htmlFor="password">Senha</label>
-        <input className="input-login" id="password" type="text" />
+        <input 
+          className="input-login" 
+          name="password" 
+          id="password" 
+          type="text" 
+          onChange={ (e) => setPassword(e.target.value) }
+        />
       </div>
       <button 
         className="sign"
         type="button"
-        onClick={ () => setRegister(!register) }
+        onClick={ handleOnClick }
       >
-        Cadastrar
+        { register === false ? 'Entrar' : 'Cadastrar' }
       </button>
     </div>
   )
--- src/components/RightLogin.jsx
+++ src/components/RightLogin.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function RightLogin() {
   const [register, setRegister] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
 
-  function handleOnClick() {
-    setRegister(!register)
+  const navigate = useNavigate();
 
-    if (register === true) {
-      alert('Usuário cadastrado com sucesso')
+  const headers = {
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    "Content-Type": "application/json",
+  };
+
+  async function handleOnClick() {
+    try {
+      const api = await fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({ email, password })
+      });
+
+      const data = await api.json();
+      
+      if (!data.token) {
+        return alert(`Erro ${api.status}: ${data.message}`)
+      }
+
+      setToken(api.token)
+
+      navigate('/user')
+
+      return;
+    } catch (error) {
+      console.log(error)
+      console.log(error)
     }
   }
 
@@ -16,11 +44,23 @@ export default function RightLogin() {
       <h2 className="welcome">{ register === false ? 'Seja bem-vindo' : 'Digite os dados' }</h2>
       <div className="inputs">
         <label className="label-input" htmlFor="user">Usuário</label>
-        <input className="input-login" id="user" type="text" />
+        <input
+          className="input-login" 
+          name="user" 
+          id="user" 
+          type="text" 
+          onChange={ (e) => setEmail(e.target.value) }
+        />
       </div>
       <div className="inputs">
         <label className="label-input" htmlFor="password">Senha</label>
-        <input className="input-login" id="password" type="text" />
+        <input 
+          className="input-login" 
+          name="password" 
+          id="password" 
+          type="password" 
+          onChange={ (e) => setPassword(e.target.value) }
+        />
       </div>
       <button 
         className="sign"

d1130fef9c952862f96602c60592b3db92cf519f
Rodolfo <rodolfoserralha@hotmail.com>
2022-03-30 20:20:58 -0300

    Feat: add content css and fix some directory errors

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import RightLogin from '../components/RightLogin';
 import LeftLogin from '../components/LeftLogin';
+import '../style/login.css';
 
 export default function Login() {
   return (
